543a9b351f3d2e903ae663c16c27d68dce5a4704
Sarah <sarahpardeike@gmail.com>
2023-10-25 15:09:20 +0200

    added test

--- Simple_Bookstore/books/tests.py
+++ Simple_Bookstore/books/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BookTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
