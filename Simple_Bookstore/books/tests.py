63967e180ddbc46451a696b6e9311c14eb599961
Sarah <sarahpardeike@gmail.com>
2023-10-25 15:14:49 +0200

    test

--- Simple_Bookstore/books/tests.py
+++ Simple_Bookstore/books/tests.py
@@ -5,3 +5,4 @@ class BookTest(TestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 302)
+        # self.assertContains(response, "h1")
